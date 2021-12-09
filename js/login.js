4949c8714762012abac5c70798299aa9ecd29cef
Gerson Joselito Reynoso Batz <ger12xela@gmail.com>
2021-12-08 23:31:50 -0600

    agregar js

---
+++ js/login.js
@@ -0,0 +1,8 @@
+function login() {
+    let usuario = document.getElementById("usuario");
+    let password = document.getElementById("password");
+    console.log(usuario.value)
+    console.log(password.value)
+
+    alert("hola " + usuario.value);
+}
