f6d0b481ca60b71a1e5b317d24eb417ebe600f1f
Gerson Joselito Reynoso Batz <ger12xela@gmail.com>
2021-12-09 21:09:18 -0600

    final

---
+++ js/registrar.js
@@ -0,0 +1,13 @@
+function registrar() {
+    let nombre = document.getElementById("nombre");
+    let correo = document.getElementById("correo")
+    let telefono = document.getElementById("telefono")
+    let password1 = document.getElementById("password1")
+    let password2 = document.getElementById("password2")
+    console.log("Cliente a registar")
+    console.log("Nombre " + nombre.value)
+    console.log("correo " + correo.value)
+    console.log("telefono " + telefono.value)
+    console.log("contraseña " + password1.value)
+    console.log("contraseña " + password2.value)
+}
